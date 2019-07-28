a4b16955be6b41f7d0e839209069f057dd3fd692
Navneet Kumar Chandel <navneet-kumar.chandel@soprasteria.com>
2019-07-28 15:04:02 +0000

    Not Found Functionality

---
+++ webapp/controller/NotFound.controller.js
@@ -0,0 +1,9 @@
+sap.ui.define([
+   "demo/ZNK02/controller/BaseController"
+], function (BaseController) {
+   "use strict";
+   return BaseController.extend("demo.ZNK02.controller.NotFound", {
+      onInit: function () {
+      }
+   });
+});
